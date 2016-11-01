9040a93f47a3d786b1c27218f4d6d227270a1b7f
Chloe <cehan16@gmail.com>
2016-11-01 19:39:52 -0400

    Day12

---
+++ Day12.java
@@ -0,0 +1,34 @@
+class Student extends Person{
+	private int[] testScores;
+    Student(String firstName, String lastName, int identification, int[] testScores){
+        super(firstName, lastName, identification);
+        this.testScores = testScores;
+    }
+    // calculate score
+    public char calculate(){
+        int average = 0;
+        for(int i = 0; i < testScores.length; i++){
+            average += testScores[i];
+        }
+        average = average / testScores.length;
+
+        if(average >= 90) {
+            return 'O'; // Outstanding
+        }
+        else if(average >= 80) {
+            return 'E'; // Exceeds Expectations
+        }
+        else if(average >= 70) {
+            return 'A'; // Acceptable
+        }
+        else if(average >= 55) {
+            return 'P'; // Poor
+        }
+        else if(average >= 40) {
+            return 'D'; // Dreadful
+        }
+        else {
+            return 'T'; // Troll
+        }
+    }
+}
