b2f9d9cd6d6d2000f06357ca46dfd9e641f5d1a9
Chloe <cehan16@gmail.com>
2016-11-08 15:04:27 -0500

    Day19

---
+++ Day19.java
@@ -0,0 +1,14 @@
+class Calculator implements AdvancedArithmetic{
+    public int divisorSum(int n){
+       int sum = 0;
+       for(int i = 1; i <= (int)Math.sqrt(n); i++) {
+           if(n%i==0){
+               sum = sum + i + n/i;
+           }
+       }
+       if((int)Math.sqrt(n)*(int)Math.sqrt(n)==n){
+           sum -= Math.sqrt(n);
+       }
+        return sum;
+    }
+}
